efca33a5c1520a273872eed582173b2f157d3178
LittleC <i@ltlec.cn>
2022-05-02 14:32:05 +0800

    :bug: remote prefix

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,16 @@ import { resolve } from 'path'
         bucket: core.getInput('BUCKET'),
         endpoint: 'oss-accelerate.aliyuncs.com'
     })
+    let remotePrefix = core.getInput('REMOTE_PREFIX') || ""
+    if (remotePrefix.endsWith("/")) {
+        // 下面 task 前缀有/了
+        remotePrefix = remotePrefix.slice(0, remotePrefix.length - 1)
+    }
     const prefix = resolve(process.env.GITHUB_WORKSPACE, core.getInput('LOCAL_PATH'))
     console.log('prefix', prefix)
     let tasks = glob.sync(`${prefix}/**/*.*`).map(v => v.substr(prefix.length))
     for (let task of tasks) {
-        const remoteName = (core.getInput('REMOTE_PREFIX') || "") + task
+        const remoteName = remotePrefix + task
         const localPath = resolve(prefix, '.' + task)
         console.log(remoteName, task, localPath)
         await store.put(remoteName, localPath)
